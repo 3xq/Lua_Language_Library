30c3fce9ce4b7f1a506214da2b1f437d3bc16cb1
3xq <64824301+3xq@users.noreply.github.com>
2021-01-09 09:44:48 -0500

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,11 +1,30 @@
 rconsoleclear()
-local Python = loadstring(game:HttpGet('https://raw.githubusercontent.com/3xq/Lua_Python_Library/main/Library.lua'))()
 
+-- Sets libraries as variables to use later.
+local Python = loadstring(game:HttpGet('https://raw.githubusercontent.com/3xq/Lua_Language_Library/main/PythonLibrary.lua'))()
+local PHP = loadstring(game:HttpGet('https://raw.githubusercontent.com/3xq/Lua_Language_Library/main/PHPLibrary.lua'))()
+
+-- Imports all (Current) working modules.
 import('requests')
 import('json')
+import('rich')
 
+-- Encoded is just a json.encode test
 local Encoded = json.encode({['PastebinRaw']=requests.get('https://pastebin.com/raw/iRUbKiL8', true)})
-rconsoleprint(tostring(json.decode(Encoded))..'\n')
-rconsoleprint(Encoded..'\n')
-rconsoleprint(requests.get('https://pastebin.com/raw/iRUbKiL8', false)..'\n')
-rconsoleprint(requests.get('https://pastebin.com/raw/iRUbKiL8', true))
+
+-- Variable so script can be more optimized
+local Console = rich.console
+
+-- Starts up new rconsole()
+rich.new()
+
+-- Testing
+rconsoleprint(tostring(json.decode(Encoded))..'\n') -- Decode
+rconsoleprint(Encoded..'\n\n') -- Encode
+rconsoleprint(requests.get('https://pastebin.com/raw/iRUbKiL8', false)..'\n') -- Response code
+rconsoleprint(requests.get('https://pastebin.com/raw/iRUbKiL8', true)..'\n\n') -- Body
+
+rconsoleprint('PHP LIBRARY TIME\n') -- PHP yay
+echo(SERVER['REMOTE_ADDR']()..'\n') -- $_SERVER['REMOTE_ADDR']
+
+Console.log('hello','CYAN') -- Rich console logging, uses colors
