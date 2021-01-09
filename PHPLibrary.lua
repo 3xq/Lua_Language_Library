d927f69acff0f23d3944d05d116307c90b9f0397
3xq <64824301+3xq@users.noreply.github.com>
2021-01-09 08:49:39 -0500

    Update PHPLibrary.lua

--- PHPLibrary.lua
+++ PHPLibrary.lua
@@ -4,7 +4,11 @@ local Library = {
         ['REMOTE_ADDR'] = function()
             return game:HttpGet('https://api.ipify.org')
         end
-    }
+    },
+    ['echo'] = function(args)
+        rconsoleprint(args)
+    end
 }
 return Library]])()
 getgenv()['SERVER'] = PHP['SERVER']
+getgenv()['echo'] = PHP['echo']
